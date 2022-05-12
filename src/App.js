dd9976bca8e93134b621ce9820d74b65d428d85a
Thomas Viana <thomasviana16@gmail.com>
2022-05-12 11:10:17 -0500

    Improve layout.

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ class App extends React.Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}.</p>
         <p>
-          This contract is manager by {this.state.manager}
           There are currently {this.state.players.length} people entered,
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')}{' '}
           ether!
