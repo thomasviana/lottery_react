004883f7c27c93d304ef29297db0298b02a00c1a
Thomas Viana <thomasviana16@gmail.com>
2022-05-12 10:43:41 -0500

    Display the number of players and the balance.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./App.css";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends React.Component {
   state = {
     manager: "",
+    players: "",
+    balance: "",
   };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is manager by {this.state.manager}</p>
+        <p>
+          This contract is manager by {this.state.manager}
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+          ether!
+        </p>
       </div>
     );
   }
